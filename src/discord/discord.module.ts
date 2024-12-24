1c9aaacf1fc349a6a362d1bb19a10772c5240ca6
stevenvo780 <stevenvallejo780@gmail.com>
2024-12-24 00:11:33 -0500

    Agregar LibraryModule a las importaciones en DiscordModule

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { DiscordService } from './discord.service';
 import { DiscordController } from './discord.controller';
 import { ConfigModule } from '../config/config.module';
+import { LibraryModule } from '../library/library.module';
 import { UserModule } from '../user/user.module';
 import { UserDiscordModule } from '../user-discord/user-discord.module';
 
 @Module({
-  imports: [ConfigModule, UserModule, UserDiscordModule],
+  imports: [ConfigModule, LibraryModule, UserModule, UserDiscordModule],
   controllers: [DiscordController],
   providers: [DiscordService],
   exports: [DiscordService],
